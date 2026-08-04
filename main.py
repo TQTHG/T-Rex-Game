fb48fe3dfd685f43834f9e50bd9ddc1c714009f7
TQTHG <tranquocthiendalat@gmail.com>
2026-08-04 14:19:15 +0700

    Add T-Rex , ground and score

--- main.py
+++ main.py
@@ -17,6 +17,29 @@ green = (0,255,0)
 white = (255,255,255)
 black = (0,0,0)
 
+# Font
+font = pygame.font.SysFont(None,30)
+
+# Score
+score = 0
+best_score = 0
+
+# Ground
+ground_y = height - 100
+ground_rect = pygame.Rect(0 , ground_y , width , height)
+
+# Speed
+frame_count = 0
+
+# T-Rex
+trex_width = 40
+trex_height = 60
+
+trex_x =  int(width * 1/5)
+trex_y =  ground_y - trex_height
+
+trex_rect = pygame.Rect(trex_x,trex_y,trex_width,trex_height)
+
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption("")
 
@@ -28,6 +51,23 @@ while running:
             running = False
 
     screen.fill(black)
+
+    ground = pygame.draw.rect(screen , white , ground_rect , 5)
+
+    trex = pygame.draw.rect(screen , green , trex_rect)
+
+    score_text = font.render(f"Score: {score}" , True , white)
+    score_rect = score_text.get_rect(midtop = (50 , 0))
+    screen.blit(score_text,score_rect)
+
+    best_text = font.render(f"Best score: {best_score}" , True , white)
+    best_rect = best_text.get_rect(midtop = (200 , 0))
+    screen.blit(best_text,best_rect)
+
+    frame_count += 1
+    if frame_count >= fps // 12:
+        score += 1
+        frame_count = 0
 
     clock.tick(fps)
     pygame.display.update()
